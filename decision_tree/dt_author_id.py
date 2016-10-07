e9d4e72c30c395d0f5ba099958d773e172d5f128
Robin Wallin <robin@twingly.com>
2016-10-07 16:00:10 +0200

    DT: Your First Email Accuracy

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -25,6 +25,13 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import tree
+
+classifier = tree.DecisionTreeClassifier(min_samples_split=40)
+
+classifier.fit(features_train, labels_train)
+
+print "accuracy:", classifier.score(features_test, labels_test)
 
 #########################################################
 
