bb609c5f0e255adf85348c8aaa03654685299abe
Robin Wallin <robin@twingly.com>
2016-11-10 17:10:49 +0100

    Regressions

--- regression/finance_regression.py
+++ regression/finance_regression.py
@@ -22,6 +22,7 @@ dictionary = pickle.load( open("../final_project/final_project_dataset_modified.
 ### list the features you want to look at--first item in the
 ### list will be the "target" feature
 features_list = ["bonus", "salary"]
+# features_list = ["bonus", "long_term_incentive"]
 data = featureFormat( dictionary, features_list, remove_any_zeroes=True)
 target, features = targetFeatureSplit( data )
 
@@ -29,7 +30,7 @@ target, features = targetFeatureSplit( data )
 from sklearn.cross_validation import train_test_split
 feature_train, feature_test, target_train, target_test = train_test_split(features, target, test_size=0.5, random_state=42)
 train_color = "b"
-test_color = "b"
+test_color = "r"
 
 
 
@@ -39,9 +40,18 @@ test_color = "b"
 ### "r" to differentiate training points from test points.
 
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(feature_train, target_train)
+
+training_score = reg.score(feature_train, target_train)
+test_score = reg.score(feature_test, target_test)
 
 
-
+print "Slope:", reg.coef_
+print "Intercept:", reg.intercept_
+print "Training score:", training_score
+print "Test score:", test_score
 
 
 
@@ -64,6 +74,11 @@ try:
     plt.plot( feature_test, reg.predict(feature_test) )
 except NameError:
     pass
+
+reg.fit(feature_test, target_test)
+plt.plot(feature_train, reg.predict(feature_train), color="b")
+print "Slope:", reg.coef_
+
 plt.xlabel(features_list[1])
 plt.ylabel(features_list[0])
 plt.legend()
