5e7481a6a3d6711cb1c0338ac74b7e1aab535a70
Robin Wallin <robin@twingly.com>
2016-11-17 16:46:25 +0100

    Outliers: Slope and score of regression w/o cleaning

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -26,8 +26,14 @@ ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+print "Slope:", reg.coef_
 
+test_score = reg.score(ages_test, net_worths_test)
+print "Test score:", test_score
 
 
 
