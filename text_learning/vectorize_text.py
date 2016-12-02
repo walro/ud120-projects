375ceaefade5964f63dda4081e38896d62911859
Robin Wallin <robin@twingly.com>
2016-12-02 16:37:40 +0100

    Text Learning

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -41,20 +41,28 @@ for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
         ### only look at first 200 emails when developing
         ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
+        # temp_counter += 1
         if temp_counter < 200:
             path = os.path.join('..', path[:-1])
             print path
             email = open(path, "r")
 
             ### use parseOutText to extract the text from the opened email
+            words = parseOutText(email)
 
             ### use str.replace() to remove any instances of the words
             ### ["sara", "shackleton", "chris", "germani"]
+            for word in ["sara", "shackleton", "chris", "germani"]:
+                words = words.replace(word, "")
 
             ### append the text to word_data
+            word_data.append(words)
 
             ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
+            if name == "sara":
+                from_data.append(0)
+            else:
+                from_data.append(1)
 
 
             email.close()
@@ -67,9 +75,16 @@ pickle.dump( word_data, open("your_word_data.pkl", "w") )
 pickle.dump( from_data, open("your_email_authors.pkl", "w") )
 
 
+print word_data[152]
 
 
 
 ### in Part 4, do TfIdf vectorization here
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+tlfid = TfidfVectorizer(stop_words = "english")
+tlfid.fit_transform(word_data)
+feature_words = tlfid.get_feature_names()
 
+print "number of features", len(feature_words)
+print feature_words[34597]
