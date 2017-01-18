cfd27aaf92b22059f15d1ac173df383d7fac14eb
Robin Wallin <robin@twingly.com>
2017-01-18 16:04:01 +0100

    Validation

--- validation/validate_poi.py
+++ validation/validate_poi.py
@@ -30,3 +30,10 @@ labels, features = targetFeatureSplit(data)
 ### it's all yours from here forward!
 
 
+from sklearn.cross_validation import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
+
+from sklearn import tree
+clf = tree.DecisionTreeClassifier()
+clf.fit(features_train, labels_train)
+print clf.score(features_test, labels_test)
